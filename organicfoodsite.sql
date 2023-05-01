8f3cfd8e8ee326b2a54b3798986a2e56edaca65e
Malik-Haziq <malikhaziq153@gmail.com>
2023-05-01 19:55:58 +0500

    update db

---
+++ organicfoodsite.sql
@@ -0,0 +1,215 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 01, 2023 at 04:52 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `organicfoodsite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pass` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `email`, `pass`) VALUES
+(1, 'admin', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `id` int(100) NOT NULL,
+  `userid` int(100) NOT NULL,
+  `productid` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `price` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `cart`
+--
+ 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(100) NOT NULL,
+  `userid` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `phone` int(100) NOT NULL,
+  `mobnumber` int(100) NOT NULL,
+  `txid` varchar(100) NOT NULL,
+  `totalproduct` varchar(100) NOT NULL,
+  `totalprice` int(100) NOT NULL,
+  `status` varchar(100) DEFAULT NULL,
+  `created_at` timestamp(6) NOT NULL DEFAULT current_timestamp(6)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE `product` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `catagory` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `quantity` int(100) NOT NULL,
+  `Price` int(100) NOT NULL,
+  `imgname` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `product`
+--
+
+INSERT INTO `product` (`id`, `name`, `catagory`, `description`, `quantity`, `Price`, `imgname`) VALUES
+(17, 'Onion', 'Vegetables', 'khuch', 17, 50, 'onion.jpg'),
+(19, 'peas green', 'Vegetables', '', 0, 5, 'peas-green.jpg'),
+(20, 'Okra', 'Vegetables', '', 34, 12, 'okra.jpg'),
+(21, 'mint leaves', 'Vegetables', '', 0, 2, 'mint-leaves.jpg'),
+(22, 'spanich', 'Vegetables', '', 3, 4, 'spinach.jpg'),
+(23, 'broccoli', 'Vegetables', '', 28, 8, 'broccoli.jpg'),
+(24, 'Tomato', 'fruits', '', 3, 34, 'tomato.jpg'),
+(25, 'Apple', 'fruits', '', 18, 12, 'apple-kalakulu.jpg'),
+(26, 'Banana', 'Fruits', '', 34, 56, 'banana.webp'),
+(27, 'Carrot', 'fruits', '', 3, 34, 'red-carrot.webp'),
+(28, 'Strawberry', 'fruits', '', 5, 34, 'strawberry.webp'),
+(29, 'Grapes', 'fruits', '', 5, 54, 'Grapes-Sundarkhani.webp'),
+(30, 'Cucumber', 'fruits', '', 8, 9, 'cucumber.webp'),
+(31, 'Guava', 'fruits', '', 8, 89, 'guava.jpg'),
+(32, 'Green capsicum', 'vegetables', '', 45, 77, 'green-capsicum.webp'),
+(33, 'Ponegranate', 'fruits', '', 54, 56, 'pomegranate-kandhari.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(100) NOT NULL,
+  `f_name` varchar(100) NOT NULL,
+  `l_name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `pass` int(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `f_name`, `l_name`, `email`, `pass`) VALUES
+(1, 'toqeer', 'abbas', 'toqeer@gmail.com', 123456),
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `product`
+--
+ALTER TABLE `product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=59;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `product`
+--
+ALTER TABLE `product`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
